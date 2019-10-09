01bef8ccec22c421015d76aee8075b671b79080f
Hectorflores20 <hector.espinozaflores@sjsu.edu>
2019-10-09 16:42:30 -0700

    noise

--- sketch.js
+++ sketch.js
@@ -1,11 +1,22 @@
+
+let noiseOffset = 0.0;
+let strokeWight = 5;
+
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(windowWidth, windowHeight);
   background(220);
+
+
+
 }
 
 function draw() {
+  background(220, 50, 133, 5);
+  strokeWight(strokeWight);
 
-  if (mouseIsPressed){
+  noiseOffset = noiseOffset + 0.01;
+  stokeWeight = noise(noiseOffset) * 10;
+
     stroke(map(mouseX, 0, 600, 0, 255, true))
     line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
     line(mouseX, mouseY, pmouseX, pmouseY);
