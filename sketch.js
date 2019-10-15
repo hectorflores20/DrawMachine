c4b582ae94bbcbedfd858ff6c16ac36fab33ee75
Hectorflores20 <hector.espinozaflores@sjsu.edu>
2019-10-15 00:55:52 -0700

    updates

--- sketch.js
+++ sketch.js
@@ -1,35 +1,34 @@
 
-let noiseOffset = 0.0;
-let strokeWidht = 5;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(220, 50, 133);
+  background(200, 50, 133);
 
-
+  strokeWeight(5);
 
 }
 
 function draw() {
-  background(220, 50, 133, 5);
-  strokeWight(strokeWidht);
 
-  noiseOffset = noiseOffset + 0.05;
-  stokeWeight = noise(noiseOffset) * 100;
-
+  if (mouseIsPressed){
     stroke(map(mouseX, 0, 600, 0, 255, true))
     line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
     line(mouseX, mouseY, pmouseX, pmouseY);
   }
 
-  function keyTyped() {
+}
 
-    if (key === 's') {
-      //save this image
-      saveCanvas('fileName', 'png');
-    } else if (key === 'c') {
-      //clear the image
-      clear();
-    }
+function keyTyped(){
+
+  if (key === 's'){
+    // save this images
+    saveCanvas('fileName', 'png');
+  } else if (key === 'd'){
+    // clear the image
+    clear();
 
   }
+
+  return false;
+
+}
